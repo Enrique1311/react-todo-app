9841fccea11869d20c69a95216eb0b548de48a66
Enrique1311 <ejspinelli131176@gmail.com>
2023-08-28 21:34:40 -0300

    Solución de detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { TodoProvider } from "./components/TodoContext";
 import TodoUI from "./components/TodoUI";
 
-// const defaultTasks = [
-// 	{ text: "nfnmbnmbmb", completed: true },
-// 	{ text: "mhjgkgk", completed: false },
-// 	{ text: "jklkljlkjklj", completed: true },
-// 	{ text: "kjkjljkjll", completed: true },
-// 	{ text: "kjkjl", completed: false },
-// ];
-
-// localStorage.setItem("tasks_1", JSON.stringify(defaultTasks));
-
 function App() {
 	return (
 		<TodoProvider>
